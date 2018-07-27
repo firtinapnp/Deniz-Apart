2191ca09370ff0285c0e9635d42a783ca3247c03
Fırtına Küçük <firtinapnp@fs0c13ty-Mac.local>
2018-07-27 10:32:32 +0300

    Some grid done

---
+++ public_html/grid.js
@@ -0,0 +1,37 @@
+var mainEl=document.getElementById("griddy");
+var rows=[];
+for(var i=0;i<3;i++){
+    rows[i]=document.createElement("div");
+    rows[i].setAttribute("class","row");
+    var cols=[];
+    for(var j=0;j<5;j++){
+        cols[j]=document.createElement("div");
+        cols[j].setAttribute("class","col m2 ");
+        if(j==0){
+            cols[j].setAttribute("class","col m2 offset-m1");
+
+        }
+        rows[i].appendChild(cols[j]);
+    }
+    mainEl.appendChild(rows[i]);
+}
+var createImageElement = function(i,j,data){
+    var img = document.createElement("img");
+    var photoid=i*5+j+1;
+    var jsonentry=data[photoid-1];
+    if(jsonentry.id==photoid){
+        img.setAttribute("src",jsonentry.link);
+    }
+    img.setAttribute("class","responsive-img materialboxed");
+    return img;
+}
+$.getJSON( "images.json", function( data ) {
+    console.log(data.images);
+    //var img = createImageElement(i,j,data.images);
+    for(var i=0;i<3;i++){
+        for(var j=0;j<5;j++){
+            var img = createImageElement(i,j,data.images);
+            console.log(rows[i].childNodes[j].appendChild(img))
+        }
+    }
+});
